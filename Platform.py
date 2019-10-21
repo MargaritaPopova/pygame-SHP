a9fe5c0d373ed3f5cefdb5d50fe14445cf76dac7
Margarita Popova <margarita.kistanova@gmail.com>
2019-10-21 12:34:05 +0300

    collision logic

--- Platform.py
+++ Platform.py
@@ -1,4 +1,5 @@
 import pygame
+from random import randint
 
 
 class Platform:
@@ -7,20 +8,30 @@ class Platform:
         self.width = screen.width//4
         self.height = 10
         self.color = 85, 107, 47
-        self.x = 0
-        self.y = screen.height - self.height
+        self.x = randint(0, screen.width)
+        self.y = randint(0, screen.height)
+        self.rect = pygame.Rect(self.x, self.y, self.width, self.height)
+
+    def recalculate_rect(self):
+        self.rect = pygame.Rect(self.x, self.y, self.width, self.height)
 
     def draw(self, screen):
-        pygame.draw.rect(screen, self.color, (self.x, self.y, self.width, self.height), 0)
+        pygame.draw.rect(screen, self.color, self.rect)
+
+    def collides_with(self, ball):
+        return self.rect.colliderect(ball.image_rect)
 
     def move_left(self):
         if self.x <= 0:
             self.x = 0
         else:
             self.x -= 50
+        self.recalculate_rect()
 
     def move_right(self, screen):
         if self.x + self.width >= screen.width:
             self.x = screen.width - self.width
         else:
             self.x += 50
+        self.recalculate_rect()
+
